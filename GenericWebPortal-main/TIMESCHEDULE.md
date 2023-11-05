f1f2bce824ddbd3760279a29ca14131d68cd0090
Tobias Gacko <e11730105@tuwien.ac.at>
2023-11-05 13:51:16 +0100

    [Django|Homepage] JSON/CSV Download

--- GenericWebPortal-main/TIMESCHEDULE.md
+++ GenericWebPortal-main/TIMESCHEDULE.md
@@ -7,4 +7,8 @@
 | 31.10.2023 | 17:50 - 18:40    | #1 Reading and creating fixture data                                                       | 50min   | 3h39min         |
 | 31.10.2023 | 20:00 - 22:00    | #2 Reading and creating fixture data                                                       | 2h      | 5h39min         |
 | 01.10.2023 | 12:00 - 18:00:00 | Finishing the DataGenerator                                                                | 6h      | 11h39min        |
+| 04.10.2023 | 13:50 - 17:00    | [Homepage] Zooming in to a picture fix                                                     | 3h10min | 14h49min        |
+| 04.10.2023 | 17:00 - 17:30    | [Homepage] Caching the theme                                                               | 30min   | 15h19min        |
+| 05.10.2023 | 08:35 - 11:30    | [Homepage] Download CSV/JSON buttons                                                       | 6h      | 18h34min        |
+| 04.10.2023 | ?                | [Homepage] Expandable/Collapsible filters + filtering columns                              |         |                 |
 
--- GenericWebPortal-main/core/staticfiles/modeling_tools/static/assets/js/modeling-tools.js
+++ GenericWebPortal-main/core/staticfiles/modeling_tools/static/assets/js/modeling-tools.js
@@ -94,7 +94,7 @@ function phoneWebModeCssSetter() {
     }
 }
 
-// TODO: Finish
+
 /***********************************
  ZOOMING IN A PICTURE
 ************************************/
@@ -147,10 +147,166 @@ const closeZoomIn = (xButton, bg, event = 'click') => {
 /***********************************
  DOWNLOADING MODELING TOOLS AS CSV/JSON
 ************************************/
+const rows = [];
+let csvRows = "data:text/csv;charset=utf-8,";
+let csvRowsArr = [];
+
 const modelingToolsRows = document.getElementById('modeling-tools-column').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
 for (let i = 0; i < modelingToolsRows.length; i++) {
     const modelingToolCells = modelingToolsRows[i].getElementsByTagName('td');
-    for (let j = 0; j < modelingToolCells.length; j++) {
-        // console.log(modelingToolCells[j]);
+
+    const mtJson =   {
+        "name": modelingToolCells[0].children[0].innerHTML.replace(/\n/g, "").trim(),
+        "link": modelingToolCells[0].children[0].getAttribute('href'),
+        "openSource": evaluateTableValue(modelingToolCells[1].innerHTML),
+        "technology": evaluateTableValue(modelingToolCells[2].innerHTML.replace(/\n/g, "").trim()),
+        "webApp": evaluateTableValue(modelingToolCells[3].innerHTML),
+        "desktopApp": evaluateTableValue(modelingToolCells[4].innerHTML),
+        "category": evaluateTableValue(modelingToolCells[5].innerHTML),
+        "modelingLanguages": evaluateTableValue(modelingToolCells[6].innerHTML.trim().replace(/\n/g, " ").replace(/\s+/g, ", ")),
+        "sourceCodeGeneration": evaluateTableValue(modelingToolCells[7].innerHTML),
+        "cloudService": evaluateTableValue(modelingToolCells[8].innerHTML),
+        "license": evaluateTableValue(modelingToolCells[9].innerHTML),
+        "loginRequired": evaluateTableValue(modelingToolCells[10].innerHTML),
+        "realTimeCollab": evaluateTableValue(modelingToolCells[11].innerHTML.trim().replace(/\n/g, " ").replace(/\s+/g, ", ")),
+        "creators": evaluateTableValue(modelingToolCells[12].innerHTML.trim().replace(/\n/g, "")),
+        "platforms": evaluateTableValue(modelingToolCells[13].innerHTML.trim().replace(/\n/g, " ").replace(/\s+/g, ", ")),
+        "programmingLanguages": evaluateTableValue(modelingToolCells[14].innerHTML.trim().replace(/\n/g, " ").replace(/\s+/g, ", "))
+    };
+    rows.push(mtJson);
+
+    let csvRowArrTmp = [];
+    for (let key in mtJson) {
+        // csvRows += mtJson[key] + (key === 'programmingLanguages' ? "\n" : ";");
+        csvRowArrTmp.push(mtJson[key]) // += mtJson[key] + (key === 'programmingLanguages' ? "\n" : ";");
     }
+    // csvRows += mtJson["name"] + ";" + mtJson["link"] + ";" + mtJson["openSource"] + "\n";
+    csvRowsArr.push(csvRowArrTmp);
+}
+console.log(csvRows);
+
+/**
+ * Evaluates a data entry from within the "modeling tools"-table and checks if it is null or empty
+ * @param value of the table cell
+ * @returns {null|*} null if the entry is empty or corresponds to a null value, otherwise returns the value
+ */
+function evaluateTableValue(value) {
+    return value === 'None' || value === '' ? null : value;
+}
+
+const csvDownload = document.getElementById('download-csv');
+const jsonDownload = document.getElementById('download-json');
+
+jsonDownload.addEventListener('click', () => {
+    const exportName = "modeling_tools_test";
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(rows));
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+});
+
+console.log(rows[0]);
+console.log(rows[0]['name']);
+
+csvDownload.addEventListener('click', () => {
+    downloadBlob(arrayToCsv(csvRowsArr), 'export.csv', 'text/csv;charset=utf-8;')
+    // download_csv_file();
+    /*
+    const jsonArray = [];
+    for (let i = 0; i < rows.length; i++) {
+        const jsonRow = []
+        for (let row in rows[i]) {
+            jsonRow.push(rows[i][row]);
+        }
+        jsonArray.push(jsonRow);
+    }
+    let csvString = "";
+    for (let i = 0; i < jsonArray.length; i++) {
+        csvString += jsonArray[i][0] + ";" + jsonArray[i][1] + "\n";
+        console.log(i);
+    }
+        console.log(jsonArray[50][0] + ";" + jsonArray[50][1]);
+     */
+    // const csvContent = "data:text/csv;charset=utf-8," + jsonArray.map(e => e.join(";")).join("\n");
+    // const csvContent = "data:text/csv;charset=utf-8," + csvRows;
+
+    /*
+    const csvContent = csvRows;
+    const encodedUri = 'data:text/csv;charset=utf-8,' + encodeURI(csvContent);
+    const link = document.createElement("a");
+
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    link.setAttribute("id", "download-csv-link");
+    document.body.appendChild(link); // Required for FF
+
+    link.click(); // This will download the data file named "my_data.csv".
+
+     */
+});
+
+//create a user-defined function to download CSV file
+function download_csv_file() {
+    const csvFileData = [
+       ['Alan Walker', 'Singer'],
+       ['Cristiano Ronaldo', 'Footballer'],
+       ['Saina Nehwal', 'Badminton Player'],
+       ['Arijit Singh', 'Singer'],
+       ['Terence Lewis', 'Dancer']
+    ];
+
+    //define the heading for each row of the data
+    let csv = 'Name,Profession\n';
+
+    //merge the data with CSV
+    /*
+    csvFileData.forEach(function(row) {
+            csv += row.join(',');
+            csv += "\n";
+    });
+     */
+    csvRowsArr.forEach(function(row) {
+            csv += row.join(',');
+            csv += "\n";
+    });
+
+    //display the created CSV data on the web browser
+    document.write(csv);
+
+
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+
+    //provide the name for the CSV file to be downloaded
+    hiddenElement.download = 'Famous Personalities.csv';
+    hiddenElement.click();
+}
+
+function arrayToCsv(data){
+  return data.map(row =>
+    row
+    .map(String)  // convert every value to String
+    .map(v => v.replaceAll('"', '""'))  // escape double quotes
+    .map(v => `"${v}"`)  // quote it
+    .join(';')  // comma-separated
+  ).join('\r\n');  // rows starting on new lines
+}
+
+/** Download contents as a file
+ * Source: https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+ */
+function downloadBlob(content, filename, contentType) {
+  // Create a blob
+  var blob = new Blob([content], { type: contentType });
+  var url = URL.createObjectURL(blob);
+
+  // Create a link to download it
+  var pom = document.createElement('a');
+  pom.href = url;
+  pom.setAttribute('download', filename);
+  pom.click();
 }
