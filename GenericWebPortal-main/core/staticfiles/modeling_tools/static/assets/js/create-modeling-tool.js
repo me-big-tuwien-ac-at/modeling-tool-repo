8fd9581357427257b4ce4281bfe0314e3b8d3445
Tobias Gacko <e11730105@tuwien.ac.at>
2024-02-08 17:44:18 +0100

    [Django|Create Modeling Tool - CSS] Duplicate name warning

--- GenericWebPortal-main/core/staticfiles/modeling_tools/static/assets/js/create-modeling-tool.js
+++ GenericWebPortal-main/core/staticfiles/modeling_tools/static/assets/js/create-modeling-tool.js
@@ -16,3 +16,27 @@ function extract_regex_matches(input, regex, group) {
   }
   return result
 }
+
+/***********************************
+ VERIFYING IF MODELING TOOL NAME ALREADY EXISTS
+************************************/
+const tool_name_input = document.getElementById('tool_name');
+tool_name_input.addEventListener('input', (event) => {
+  const user_tool_name = event.target.value;
+  const nameDuplicateWarning = document.getElementById('invalid-input-name-duplicate');
+  let name_match = false;
+
+  for (let i = 0; i < modeling_tools.length; i++) {
+    if (modeling_tools[i].trim().toLowerCase() === user_tool_name.toLowerCase()) {
+      name_match = true;
+      break;
+    }
+  }
+  if (name_match) {
+    nameDuplicateWarning.style.display = null;
+    tool_name_input.classList.add('invalid-input-border');
+  } else {
+    nameDuplicateWarning.style.display = 'none';
+    tool_name_input.classList.remove('invalid-input-border');
+  }
+});
